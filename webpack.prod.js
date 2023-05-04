8df4d4a9c436b8ad4af8154b1259e7a50b3c74fa
InsKill <tm_adm@mail.ru>
2023-05-04 14:37:51 +0600

    xcvx

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,26 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
 
 // webpack.production.config.js
 module.exports = {
-    mode: 'production',
-    entry: './src/new.ts',
+    mode: "production",
+    entry: "./src/new.ts",
     output: {
-      filename: 'main.js'
+      filename: "main.js"
     },
     devServer: {
-      static: './dist',
+      static: "./dist",
       open: true
     },
-    stats: 'errors-only',
+    stats: "errors-only",
     plugins: [ new MiniCssExtractPlugin(),
                new HtmlWebpackPlugin({
-                template: './src/index.pug',
-                title: 'Development',
+                template: "./src/index.pug",
+                title: "Development",
               }),
                new TerserWebpackPlugin(),
                new CssMinimizerPlugin(),
@@ -35,19 +35,19 @@ module.exports = {
           options: {
             esModule: true,
           }
-        }, 'css-loader'],
+        }, "css-loader"],
         test: /\.css$/
       },
       {
         test: /\.pug$/,
-        loader: 'pug-loader',
+        loader: "pug-loader",
         options: {
           pretty: true,
         }
       },
       {
         test: /\.ts$/,
-        use: 'ts-loader'
+        use: "ts-loader"
       },
     ]
     },
